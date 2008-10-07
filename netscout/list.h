5568a421fb1838bbdd91a62ed09253dd774699ab
Martin Kupec <magon@kam.mff.cuni.cz>
2008-10-07 15:29:44 +0200

    Added missing headers

--- netscout/list.h
+++ netscout/list.h
@@ -2,6 +2,7 @@
 #define __NETSCOUT_LIST_H__
 
 #include <stdlib.h>
+#include <string.h>
 
 #define list_ether_add_uniq(uniq) ((struct stat_ether *) (list_add_uniq(&list_ether, sizeof(struct stat_ether), (uint8_t *) uniq, 6) ))
 #define list_ip_add_uniq(uniq) ((struct stat_ip *) (list_add_uniq(&list_ip, sizeof(struct stat_ip), (uint8_t *) &uniq, 4) ))
